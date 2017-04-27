44255425f2f7a31d3e9eba7c7725469cd38e7d93
Leonard Halling <leonardh@kth.se>
2017-04-27 16:50:51 +0200

    text klient

--- labb03/src/textClient.java
+++ labb03/src/textClient.java
@@ -1,16 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * Created by Leonard on 2017-04-27.
  */
 public class textClient {
 
     private void connect(){
-        try {
-            Socket socket=new Socket("gru-ld03.csc.kth.se",4713);
+        try{
+            Socket socket = new Socket("share-02.csc.kth.se", 4713);
             BufferedReader in=new BufferedReader
                     (new InputStreamReader(socket.getInputStream()));
             PrintWriter ut=new PrintWriter(socket.getOutputStream());
             ut.println("Charlotta"); ut.flush();
-            System.out.println(in.readLine());
+            for (int i = 0; i < 2; i++) {
+                ut.println("SAX"); ut.flush();
+                System.out.println(in.readLine());
+            }
+        }catch (IOException error) {
+            System.out.println("Fel vid inläsning");
         }
+
+    }
+
+    public static void main(String[] args) {
+        textClient tc = new textClient();
+        tc.connect();
     }
 }
